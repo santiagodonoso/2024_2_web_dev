94401a6fa474ce440756993207d2b09a70b18f5e
Santiago Donoso <santiagodonoso@gmail.com>
2024-09-03 10:51:45 +0200

    ...

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from bottle import get, run, template, static_file
+from bottle import get, run, template, static_file, post, put, delete
 
 ##############################
 @get("/") # decorator
@@ -15,8 +15,57 @@ def _():
 def _():
     return static_file("app.js", ".")
 
+# API
+##############################
+"""
+CREATE READ UPDATE DELETE ALSO KNOWN AS THE CRUD
+
+HTTP METHOD GET to READ data
+/items
+
+HTTP METHOD POST TO CREATE DATA
+/items
+
+HTTP METHOD PUT TO UPDATE DATA
+/items/<id>
+
+HTTP METHOD DELETE TO DELETE DATA
+/items/<id>
 
 
+
+"""
+
+##############################
+@get("/items")
+def _():
+    return "xxxxxxxx"
+
+##############################
+@get("/items/<id>")
+def _(id):
+    # dictionary
+    item = {
+        "id" : 1,
+        "name" : "a"
+    }
+    return item
+
+##############################
+@post("/items")
+def _():
+    return "items created"
+
+##############################
+# f string
+@put("/items/<id>")
+def _(id):
+    return f"item {id} updated"
+
+##############################
+@delete("/items/<id>")
+def _(id):
+    return f"item {id} deleted"
 
 ##############################
 run(host="0.0.0.0", port=80, debug=True, reloader=True, interval=0.3)
